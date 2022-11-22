f55564f28b627146f44292f4a9085fcf403186f1
Victor Dorobantu <victor.d.dorobantu@gmail.com>
2022-11-22 13:40:06 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='spherical_generative_modeling',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'potpourri3d',
+        'trimesh'
+    ]
+)
