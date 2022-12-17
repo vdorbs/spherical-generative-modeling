53f1005db1beef753f0fa074d4f796e5eaa4c735
charbor <cborcherds@caltech.edu>
2022-12-16 16:20:14 -0800

    add faster layout code and helper functions

--- spherical_generative_modeling/parametrization.py
+++ spherical_generative_modeling/parametrization.py
@@ -1,4 +1,4 @@
-from numpy import arange, arccos, array, concatenate, cos, diff, exp, float64, identity, log, logical_and, maximum, minimum, ones_like, pi, sin, sort, stack, tan, unique, zeros, zeros_like
+from numpy import arange, arccos, array, concatenate, cos, diff, exp, float64, identity, log, logical_and, maximum, minimum, ones_like, pi, sin, sort, stack, tan, unique, zeros, zeros_like, dot
 from numpy.linalg import norm, solve
 from numpy.typing import NDArray
 from scipy.sparse import coo_matrix, dia_matrix
@@ -138,6 +138,57 @@ def intrinsically_flatten(
     new_edge_lengths = exp(log_factor_averages) * edge_lengths
 
     return log_factors, new_edge_lengths
+  
+
+def check_length(pos0: NDArray[float], 
+                 pos1: NDArray[float], 
+                 target_length: float, 
+                 epsilon: float =1e-6):
+    """Check that the length between `pos0` and `pos1` is within `epsilon` error of the `target_length` (via assertion).
+    
+    :param pos0: (num_vertices,) first vertex position
+    :param pos1: (num_vertices,) second vertex position
+    :param target_length: what the length of the edge between `pos0` and `pos1` should be
+    : returns: None
+    """
+    actual_length = norm(pos0 - pos1)
+    assert norm(actual_length - target_length) < epsilon, f"bad length. actual: {actual_length}, target: {target_length}"
+
+
+def check_angle(vec0: NDArray[float], 
+                vec1: NDArray[float], 
+                target_angle: float, 
+                epsilon: float = 1e-6):
+    """Check that the length between `vec0` and `vec1` is within `epsilon` error of `target_angle` via assertion.
+    
+    :param vec0: (num_vertices,) first vector
+    :param vec1: (num_vertices,) second vector
+    :param target_angle: what the angle between `vec0` and `vec1` should be.
+    : returns: None
+    """
+    actual_angle = arccos(dot(vec0 / norm(vec0), vec1 / norm(vec1)))
+    assert abs(actual_angle - target_angle) < epsilon, f"bad angle. actual: {actual_angle}, target: {target_angle}"
+
+def get_angle(l_left: float, 
+              l_opp: float, 
+              l_right: float):
+    """Get the angle between the edges with lengths `l_left` and `l_right`.
+    
+    :param l_left: the length of the vector on the "left" side of the angle
+    :param l_right: the length of the vector on the "right" side of the angle
+    :param l_opp: the length of the vector on the opposite side of the angle (so its facing it)
+    : returns: (float) the angle
+    """
+    angle = arccos((-l_opp**2 + l_left**2 + l_right**2) / (2*l_left*l_right))
+    return angle % pi
+
+def get_rotation_matrix(rotation_angle: float) -> NDArray[float]:
+    """Get the matrix that rotates a vector of length 2 by `rotation_angle` (counterclockwise).
+    
+    :rotation_angle: the angle to rotate by
+    : returns: (2, 2) the rotation matrix
+    """
+    return array([[cos(rotation_angle), -sin(rotation_angle)], [sin(rotation_angle), cos(rotation_angle)]])
 
 
 def extrinsically_layout(
@@ -148,7 +199,6 @@ def extrinsically_layout(
         verbose: bool = False
 ) -> NDArray[float64]:
     """ Compute vertex positions for flat mesh from edge lengths
-
     :param num_vertices: number of vertices
     :param faces: (num_faces, 3) array of vertices comprising each face
     :param edge_lengths: (num_faces, 3) array of edge lengths for each face
@@ -156,22 +206,10 @@ def extrinsically_layout(
     :param verbose: print layout details
     :return: (num_vertices, 2) array of vertex positions in the plane
     """
-
-    assert faces.shape == edge_lengths.shape
-    assert len(faces.shape) == 2
-    assert faces.shape[-1] == 3
-    assert faces.min() == 0
-    assert faces.max() == num_vertices - 1
-    assert edge_lengths.min() >= 0.
-    assert starting_idx >= 0
-    assert starting_idx < len(faces)
-
-    num_faces = len(faces)
-    vertex_positions = zeros((num_vertices, 2))
-    is_positioned = zeros(num_vertices, dtype=bool)
-    is_visited = zeros(num_faces, dtype=bool)
-
-    # Compute interior angles
+    
+    """
+    Step 1: Compute angles
+    """
     edge_lengths_extra = concatenate([edge_lengths, edge_lengths[:, :2]], axis=-1)
     l_ijs = edge_lengths_extra[:, :3]
     l_jks = edge_lengths_extra[:, 1:4]
@@ -183,77 +221,118 @@ def extrinsically_layout(
     angles = arccos(cos_angles)
     # Switch to ijk order
     angles = angles[:, array([1, 2, 0])]
+    
+    """
+    Step 2: Create mappings for the mesh layout.
+    
+    An "edge" here is a tuple `(i, j)` where `i` and `j` are indices
+    (i.e. the indices are elements of `kept_faces`)
+    """
+    edge_to_opposite = {} # map the edge (i, j) to k, where k is the index opposite to the edge (i, j)
+    edge_to_length = {} # map the edge (i, j) to l, where l is the length of the edge of (i, j)
+    edge_to_angle = {} # map the edge (i, j) to the angle opposite to the edge between thos vertices
 
-    # Initial face
-    face = faces[starting_idx]
-    i, j, k = face
-    l_ij, l_jk, l_ki = edge_lengths[starting_idx]
-    alpha_i, alpha_j, alpha_k = angles[starting_idx]
+    for face_idx, (cur_face, cur_edge_lengths, cur_angles) in enumerate(zip(faces, edge_lengths, angles)):
+        for (i0, i1, i2), edge_length in zip([(0, 1, 2), (1, 2, 0), (2, 0, 1)], cur_edge_lengths):
+            
+            edge = (cur_face[i0], cur_face[i1])
+            assert edge not in edge_to_opposite, "edge was already used"
+            edge_to_opposite[edge] = cur_face[i2]
+            edge_to_length[edge] = edge_length
+            edge_to_angle[edge] = cur_angles[i2]
+            
+            
+    """
+    Step 3: layout the mesh
 
-    # Place vertex i at the origin, vertex j directly up, and vertex k in CCW order
-    # Assumes starting face has no edges on the boundary
-    vertex_positions[j] = array([0., l_ij])
-    vertex_positions[k] = l_ki * array([-sin(alpha_i), cos(alpha_i)])
-    is_positioned[face] = True
-    is_visited[starting_idx] = True
+    Procedure:
 
-    # Find neighboring faces
-    shared_vertex_mask = (faces.reshape(-1, 3, 1) == face.reshape(1, 1, 3)).any(axis=1)
-    shares_edge = shared_vertex_mask.sum(axis=-1) == 2
-    assert shares_edge.sum() == 3  # Assumes starting face has no edges on the boundary
-    adjacent_idxs = arange(num_faces)[shares_edge]
+    create the first edge and add it to `queue`
+    while `queue` is not empty:
+        1) edge <- first element in `queue` (pop it from `queue)
+        2) start, end <- edge (`start` and `end` are the two vertices in `edge`)
+        3) find `opp` (the opposite vertex) from `start` and `end` using the input edge lengths
+           This is identical to the procedure in the original layout code 
+           i.e. we compute the new position in two ways and then take the mean
+        4) add the edges (opp, start) and (end, opp) to the front of `queue`
+    """
+    vertex_positions = zeros(shape=(num_vertices, 2))
 
-    # Recursively place remaining faces
-    next_frontier = adjacent_idxs
-    while len(next_frontier) > 0:
-        if verbose:
-            print(is_positioned.sum(), num_vertices, is_visited.sum(), num_faces, len(next_frontier))
+    # create the first edge
+    i0, i1, _ = faces[starting_idx]
+    vertex_positions[i0] = [0., 0.]
+    vertex_positions[i1] = [0., edge_to_length[(i0, i1)]]
 
-        frontier = next_frontier
-        next_frontier = array([], dtype=int)
-        for face_idx in frontier:
-            if ~is_visited[face_idx]:
-                face = faces[face_idx]
-                local_is_positioned = is_positioned[face]
-                # A face is only in the frontier if a neighboring face had all vertices positioned
-                assert local_is_positioned.sum() >= 2
+    # initialize `queue` and `seen`
+    queue = []
+    queue.append((i0, i1))
+    queue.append((i1, i0))
+    seen = set()
 
-                # A face can be unvisited but have all vertices positioned if all neighbors were previously visited
-                if local_is_positioned.sum() == 2:
-                    local_vertex_positions = vertex_positions[face]  # Exactly one of these is not positioned
-                    local_edge_lengths = edge_lengths[face_idx]
-                    local_angles = angles[face_idx]
+    i = 0
+    while queue:
 
-                    # Classify vertices
-                    idx_orders = array([[1, 2, 0], [2, 0, 1], [0, 1, 2]])
-                    start, end, unpositioned = idx_orders[~local_is_positioned][0]
-                    
-                    edge_vector = local_vertex_positions[end] - local_vertex_positions[start]
-                    edge_vector /= norm(edge_vector)
-                    rotation_angle = local_angles[start]
-                    rotation_matrix = array([
-                        [cos(rotation_angle), -sin(rotation_angle)],
-                        [sin(rotation_angle), cos(rotation_angle)]
-                    ])
-                    new_position = local_vertex_positions[start] + rotation_matrix @ (local_edge_lengths[unpositioned] * edge_vector)
+        edge = queue.pop(0)
 
-                    vertex_positions[face[unpositioned]] = new_position
-                    is_positioned[face[unpositioned]] = True
+        if (edge in edge_to_opposite) and (edge not in seen):
 
-                # Find neighboring faces
-                shared_vertex_mask = (faces.reshape(-1, 3, 1) == face.reshape(1, 1, 3)).any(axis=1)
-                shares_edge = shared_vertex_mask.sum(axis=-1) == 2
-                assert shares_edge.sum() > 0
-                assert shares_edge.sum() <= 3
-                explorable = logical_and(shares_edge, ~is_visited)
-                adjacent_idxs = arange(num_faces)[explorable]
+            i += 1
 
-                next_frontier = concatenate([next_frontier, adjacent_idxs])
-                is_visited[face_idx] = True
+            # retrieve vertex indices
+            opp = edge_to_opposite[edge] # this is the (index of) the unpositioned vertex (opposite to `start` and `end`)
+            start, end = edge # these are the indices for the already positioned vertices
 
-    assert is_visited.all()
-    return vertex_positions
+            # retrieve vector lengths
+            l_start = edge_to_length[(opp, start)]
+            l_end = edge_to_length[(end, opp)]
 
+            # retrieve positions
+            pos_start = vertex_positions[start]
+            pos_end = vertex_positions[end]
+
+            # retrieve angles
+            angle_start = edge_to_angle[(end, opp)]
+            angle_end = edge_to_angle[(opp, start)]
+
+            # get position using vector from `start` to `opp`
+            edge_vector = pos_end - pos_start
+            edge_vector /= norm(edge_vector)
+            rotation_matrix = get_rotation_matrix(angle_start)
+            new_pos0 =  pos_start + rotation_matrix @ (l_start*edge_vector)
+
+            # get position using vector from `end` to `opp`
+            edge_vector = pos_start - pos_end
+            edge_vector /= norm(edge_vector)
+            rotation_matrix = get_rotation_matrix(angle_end).T # rotate it clockwise (for rotation matrices, the transpose is the inverse)
+            new_pos1 = pos_end + rotation_matrix @ (l_end * edge_vector)
+
+            # Set the position of `opp` (the new position)
+            pos_opp = (new_pos1 + new_pos0) / 2
+            vertex_positions[opp] = pos_opp
+
+            # Report
+            l_target = edge_to_length[edge] # target length between start and end
+            l_actual = norm(vertex_positions[end] - vertex_positions[start]) # actual length between start and end
+            if verbose and i % 1000 == 0: print(f'extrinsically_layout: iteration {i}, actual length: {l_actual}, target length: {l_target}')
+
+            # Check angles and length vs targets (via assertions)
+            assert norm(new_pos1 - new_pos0) < 1e-6, f"Positions don't match: {new_pos1}, {new_pos0}"
+            check_angle(pos_opp - pos_start, pos_end - pos_start, angle_start)
+            check_length(pos_start, pos_end, l_target)
+            check_length(pos_start, pos_opp, l_start)
+            check_length(pos_end, pos_opp, l_end)
+
+            # Update `queue`: add new edges (via depth-first)
+            queue.insert(0, (start, opp))
+            queue.insert(0, (opp, end))
+
+            # Update `seen` (edges we have already seen)
+            seen.add((opp, start))
+            seen.add((end, opp))
+            seen.add(edge)
+            
+    return vertex_positions  
+  
 
 def stereographically_project(planar_vertices: NDArray[float64]) -> Tuple[NDArray[float64], NDArray[float64]]:
     """ Stereographically project planar vertices onto unit sphere through north pole
